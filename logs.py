046214d25288334bf25bb998d897a2140b49f27d
lady-ada <45360690+lady-ada@users.noreply.github.com>
2021-01-02 17:51:24 +0530

    Update logs.py

--- logs.py
+++ logs.py
@@ -9,6 +9,7 @@ name = "bm-best-model"
 
 # load existing web service
 service = Webservice(name=name, workspace=ws)
+service.update(enable_app_insights=True)
 logs = service.get_logs()
 
 for line in logs.split('\n'):
